9b77ae79d6ff69049edc7e6f4201d773349d7bf1
Peterson Medina <opetersonmedina@outlook.com>
2021-02-08 20:00:28 -0300

    feat: finished appointments

---
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { classToClass } from 'class-transformer';
+import { container } from 'tsyringe';
+
+import ListProviderDayAvailabilityService from '@modules/appointments/services/ListProviderDayAvailabilityService';
+
+export default class ProvidersDayAvailabiliryController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+    const { day, month, year } = request.body;
+
+    const listProviderDayAvailability = container.resolve(
+      ListProviderDayAvailabilityService,
+    );
+
+    const availability = await listProviderDayAvailability.execute({
+      provider_id,
+      day,
+      month,
+      year,
+    });
+
+    return response.json({ providers: classToClass(availability) });
+  }
+}
---
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -0,0 +1,26 @@
+import { inject, injectable } from 'tsyringe';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+import IUsersRespository from '@modules/users//repositories/IUsersRepository';
+
+interface IRequest {
+  user_id: string;
+}
+
+@injectable()
+class ListProvidersService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRespository,
+  ) {}
+
+  public async execute({ user_id }: IRequest): Promise<User[]> {
+    const users = await this.usersRepository.findAllProviders({
+      except_user_id: user_id,
+    });
+
+    return users;
+  }
+}
+
+export default ListProvidersService;
